a0f36ebdd8fbd2a6b72010f033ab0db3df6127ac
iderex <30603423+iderex@users.noreply.github.com>
2017-08-02 08:10:57 +0200

    Meteor.js To do List App
    Meteor.js To do List App

---
+++ client/main.js
@@ -0,0 +1,41 @@
+import { Template } from 'meteor/templating';
+import { Notes } from '../lib/collections.js';
+import { Accounts } from 'meteor/accounts-base';
+
+Accounts.ui.config({
+  passwordSignupFields:'USERNAME_ONLY'
+});
+
+import './main.html';
+
+Template.body.helpers({
+
+  notes(){
+    return Notes.find({});
+  }
+});
+
+Template.add.events({
+  'submit .add-form': function(){
+    event.preventDefault();
+
+    const target = event.target;
+    const text = target.text.value;
+
+
+    Meteor.call('notes.insert', text);
+
+    target.text.value = '';
+
+    $('#addModal').modal('close');
+
+    return false;
+  }
+});
+
+Template.note.events({
+  'click .delete-note':function(){
+    Meteor.call('notes.remove', this);
+    return false;
+  }
+});
